838bd16b4fcb6aee7d5fe8667bc08cb53782e302
Luis Carlos Pando <hey@luiscarlospando.com>
2023-05-03 18:26:47 -0600

    Quitar useState

--- src/pages/Vinos.js
+++ src/pages/Vinos.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link } from "react-router-dom";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
